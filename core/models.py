d0d89fc1b2dc334322628f2db088671ae29ef49c
arna <57180399+Arnav-Bansal@users.noreply.github.com>
2021-03-27 11:37:16 +0530

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+
+   username = models.CharField(max_length = 50)
+   mail = models.CharField(max_length = 50)
+   name = models.CharField(max_length = 50)
+   password = models.CharField(max_length = 50)
+
+   class Meta:
+      db_table = "user"
