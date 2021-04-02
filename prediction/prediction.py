f96f0a31710ac17d64006828d4d53e69893f65dc
AllergySnipe <39958084+AllergySnipe@users.noreply.github.com>
2021-04-02 12:18:24 +0530

    intial tracker.py commit

--- prediction/prediction.py
+++ prediction/prediction.py
@@ -7,23 +7,23 @@ from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 
 df = pd.read_csv("csv/mytest.csv")
-
-train_data, test_data = df[0:int(len(df)*0.7)], df[int(len(df)*0.7):]
-training_data = train_data['Price'].values
-test_data = test_data['Price'].values
-history = [x for x in training_data]
-model_predictions = []
-N_test_observations = len(test_data)
-for time_point in range(N_test_observations):
-    model = ARIMA(history, order=(4,1,0))
-    model_fit = model.fit(disp=0)
-    output = model_fit.forecast()
-    yhat = output[0]
-    model_predictions.append(yhat)
-    true_test_value = test_data[time_point]
-    history.append(true_test_value)
-MSE_error = mean_squared_error(test_data, model_predictions)
-#print('Testing Mean Squared Error is {}'.format(MSE_error))
+def predictionFunc(df):
+    train_data, test_data = df[0:int(len(df)*0.7)], df[int(len(df)*0.7):]
+    training_data = train_data['Price'].values
+    test_data = test_data['Price'].values
+    history = [x for x in training_data]
+    model_predictions = []
+    N_test_observations = len(test_data)
+    for time_point in range(N_test_observations):
+        model = ARIMA(history, order=(4,1,0))
+        model_fit = model.fit(disp=0)
+        output = model_fit.forecast()
+        yhat = output[0]
+        model_predictions.append(yhat)
+        true_test_value = test_data[time_point]
+        history.append(true_test_value)
+    MSE_error = mean_squared_error(test_data, model_predictions)
+    #print('Testing Mean Squared Error is {}'.format(MSE_error))
 
 test_set_range = df[int(len(df)*0.7):].index
 plt.plot(test_set_range, model_predictions, color='blue', marker='o', linestyle='dashed',label='Predicted Price')
