863ed7e073e2ccf7efb8abac7eabe7c8024b1d24
arnav <arnav.guru@gmail.com>
2021-04-19 16:19:32 +0530

    Models Updated

--- Item-price-forecasting/settings.py
+++ Item-price-forecasting/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'authentication',
-    'jwt', #pyjwt
+    'prediction',
+    'core',
+    'jwt' #pyjwt
 
 ]
 
--- core/models.py
+++ core/models.py
@@ -2,22 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class user(models.Model):
-
-   username = models.CharField(max_length = 50)
-   mail = models.CharField(max_length = 50)
-   name = models.CharField(max_length = 50)
-   password = models.CharField(max_length = 50)
-
-   class Meta:
-      db_table = "user"
-
-class products(models.Model):
+class saste(models.Model):
 
    username = models.CharField(max_length = 50)
    productname = models.CharField(max_length = 50)
-   website = models.CharField(max_length = 50)
-   link = models.CharField(max_length = 50)
+   domain = models.CharField(max_length = 50)
+   pid = models.CharField(max_length = 50)
 
    class Meta:
-      db_table = "products"
+      db_table = "saste"
+
+
