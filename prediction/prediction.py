af2fa986648c04bd4c0a82f9cdba65ced67b2a56
Sparsh Agarwal <2018csb1126@iitrpr.ac.in>
2021-04-23 19:19:54 +0530

    Fixed a bug in GET request for predicted prices

--- prediction/prediction.py
+++ prediction/prediction.py
@@ -1,5 +1,5 @@
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from pandas.plotting import lag_plot
 import datetime as dt
@@ -8,9 +8,11 @@ from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 
 #df = pd.read_csv("csv/mytest.csv")
+
+
 def predictionFunc(train_data):
     #training_data, test_data = df[0:int(len(df)*0.7)], df[int(len(df)*0.7):]
-    training_data = [detail["price"] for detail in train_data] 
+    training_data = [detail["price"] for detail in train_data]
     #test_data = test_data['Price'].values
     history = [x for x in training_data]
     model_predictions = []
@@ -19,12 +21,12 @@ def predictionFunc(train_data):
     last_day = (old_dates[(len(old_dates)-1):])[0]
     new_dates = []
     for i in range(7):
-        added_day = last_day + dt.timedelta(days = (i+1))
+        added_day = last_day + dt.timedelta(days=(i+1))
         new_dates.append(added_day)
     #N_test_observations = len(test_data)
     N_test_observations = 7
     for time_point in range(N_test_observations):
-        model = ARIMA(history, order=(2,1,0))
+        model = ARIMA(history, order=(1, 0, 0))
         model_fit = model.fit(disp=0)
         output = model_fit.forecast()
         yhat = output[0]
@@ -32,7 +34,8 @@ def predictionFunc(train_data):
         #true_test_value = test_data[time_point]
         history.append(yhat)
     for i in range(len(model_predictions)):
-        jsondata.append({"date": new_dates[i],"price": model_predictions[i][0]})
+        jsondata.append(
+            {"date": new_dates[i], "price": model_predictions[i][0]})
     #MSE_error = mean_squared_error(test_data, model_predictions)
     return jsondata
     #print('Testing Mean Squared Error is {}'.format(MSE_error))
@@ -41,9 +44,8 @@ def predictionFunc(train_data):
 #plt.plot(test_set_range, model_predictions, color='blue', marker='o', linestyle='dashed',label='Predicted Price')
 #plt.plot(test_set_range, test_data, color='red', label='Actual Price')
 #plt.title('Product Prices Prediction')
-#plt.xlabel('Date')
-#plt.ylabel('Prices')
+# plt.xlabel('Date')
+# plt.ylabel('Prices')
 #plt.xticks(np.arange(881,1259,50), df.Date[881:1259:50])
-#plt.legend()
-#plt.show()
-
+# plt.legend()
+# plt.show()
